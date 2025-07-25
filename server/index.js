f2832061060810066de789875ee0c2fdb93c05f6
Chathushki Asmada <160525116+chathushkiupulini@users.noreply.github.com>
2025-07-25 15:03:39 +0530

    connected mongoDB

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './config/database.js';
+
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+const PORT = process.env.PORT || 5000
